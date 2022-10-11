0ef0088adc8b7fa7a50d9afa8c425ac57ee330a6
Dev-Shifat <shahriar.ahmed.7878@gmail.com>
2022-10-11 11:21:57 +0600

    Router/Nav Added

---
+++ src/components/Statics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statics = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Statics;
---
+++ src/util/router.js
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from '../components/Root'
+
+const router = createBrowserRouter([
+    {
+        path: '/', element: <Root />
+    }
+])
+export default router;
