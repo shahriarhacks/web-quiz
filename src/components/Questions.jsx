5fbfeec0a84427e93cf7db493240e9de30520f5b
Dev-Shifat <shahriar.ahmed.7878@gmail.com>
2022-10-12 08:17:04 +0600

    Main Marks Completed

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Options from './Options';
+import { EyeIcon } from '@heroicons/react/24/solid'
+import { toast } from 'react-toastify';
 
 const Questions = ({ qs }) => {
     const { question, options } = qs;
+    const handelClickButton = qs => {
+        toast(`The answer is:  ${qs?.correctAnswer}`)
+    }
+    const answer = (option) => {
+        if (option === qs?.correctAnswer) {
+            toast.success("Hurrah! Great Your Answer is Right")
+        }
+        else {
+            toast.warning("Alas! Your Answer wrong")
+        }
+    }
     return (
         <div>
             <div className='bg-gray-100 p-6 rounded shadow-lg'>
                 <h1 className='text-xl text-center'>{question}</h1>
+                <button onClick={() => handelClickButton(qs)}>
+                    <EyeIcon className="h-6 w-6 my-5 text-gray-500" />
+                </button>
                 <div className='grid grid-cols-1 gap-5 my-5 lg:grid-cols-2 mx-auto'>
                     {
-                        options.map((option, idx) => <Options key={idx} option={option} />)
+                        options.map((option, idx) => <Options key={idx} answer={answer} id={idx} option={option} />)
                     }
                 </div>
             </div>
