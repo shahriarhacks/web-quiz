33ac5b53c3cda49aeaae93417ccb3aa32bfbbced
Dev-Shifat <shahriar.ahmed.7878@gmail.com>
2022-10-12 10:22:25 +0600

    Chart Resposive Done

--- src/components/Statics.jsx
+++ src/components/Statics.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const Statics = () => {
     const allData = useLoaderData();
     const data = (allData.data);
     return (
         <div className='flex justify-center items-center my-24 w-full h-full'>
-            <BarChart height={300} width={500} data={data}>
-                <XAxis dataKey="name" stroke="#8884d8" />
-                <YAxis />
-                <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
-                <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <Bar dataKey="total" fill="#8884d8" barSize={30} />
-            </BarChart>
+            <ResponsiveContainer width='90%' height={400}>
+                <BarChart data={data}>
+                    <XAxis dataKey="name" stroke="#8884d8" />
+                    <YAxis />
+                    <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+                    <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="total" fill="#8884d8" barSize={30} />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
